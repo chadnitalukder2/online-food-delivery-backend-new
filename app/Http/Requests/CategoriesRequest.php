ae9cd3ae5a8e80e3d0b9f8a70bf460af933b7559
chadnitalukder2 <chadnitalukder2@gmail.com>
2024-12-02 17:27:21 +0600

    update category request

--- app/Http/Requests/CategoriesRequest.php
+++ app/Http/Requests/CategoriesRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CartRequest extends FormRequest
+class CategoriesRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
